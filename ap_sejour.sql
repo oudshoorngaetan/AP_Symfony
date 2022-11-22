b103bc68b99060a453412e1c5fb283739ed37c55
oudshoorngaetan <gaetan.oudshorn@gmail.com>
2022-11-22 17:14:10 +0100

    affichage de tous les séjours et debut

--- ap_sejour.sql
+++ ap_sejour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 22 nov. 2022 à 14:32
+-- Généré le : mar. 22 nov. 2022 à 16:13
 -- Version du serveur : 10.5.16-MariaDB
 -- Version de PHP : 8.1.12
 
@@ -11,20 +11,31 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Déchargement des données de la table `service`
+-- Base de données : `ap_sejour`
 --
 
-INSERT INTO `service` (`id`, `libelle`) VALUES
-(1, 'Cadiologie'),
-(2, 'Radiologie'),
-(3, 'Chirurgie'),
-(4, 'Chirurgie esthétique'),
-(5, 'Oncologie'),
-(6, 'Soins intensifs'),
-(7, 'Urologie'),
-(8, 'Anesthésiologie');
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `chambre`
+--
+
+CREATE TABLE `chambre` (
+  `id` int(11) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `service_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `chambre`
+--
 
 INSERT INTO `chambre` (`id`, `numero`, `service_id`) VALUES
 (1, 101, 1),
@@ -41,7 +52,23 @@ INSERT INTO `chambre` (`id`, `numero`, `service_id`) VALUES
 (12, 601, 6),
 (13, 701, 7),
 (14, 702, 7);
-/*
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
 ('DoctrineMigrations\\Version20221108154754', '2022-11-22 13:30:31', 804),
 ('DoctrineMigrations\\Version20221112135352', '2022-11-22 13:30:32', 399),
@@ -50,7 +77,18 @@ INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_
 ('DoctrineMigrations\\Version20221112143301', '2022-11-22 13:30:33', 125),
 ('DoctrineMigrations\\Version20221112144603', '2022-11-22 13:30:33', 775),
 ('DoctrineMigrations\\Version20221115144558', '2022-11-22 13:30:34', 725);
-*/
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lit`
+--
+
+CREATE TABLE `lit` (
+  `id` int(11) NOT NULL,
+  `chambre_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 --
 -- Déchargement des données de la table `lit`
 --
@@ -91,6 +129,38 @@ INSERT INTO `lit` (`id`, `chambre_id`) VALUES
 (33, 14),
 (34, 14);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `available_at` datetime NOT NULL,
+  `delivered_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `patient`
+--
+
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telephone` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adresse` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ville` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cp` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 --
 -- Déchargement des données de la table `patient`
 --
@@ -101,21 +171,247 @@ INSERT INTO `patient` (`id`, `nom`, `prenom`, `telephone`, `adresse`, `ville`, `
 (3, 'Laframboise', 'Emmanuelle', '0644814323', '65 boulevard de lille', 'Roubaix', '59100'),
 (4, 'Gaillard', 'George', '0612354523', '5 avenue des anges', 'Leers', '59150');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personnel`
+--
+
+CREATE TABLE `personnel` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sejour`
+--
+
+CREATE TABLE `sejour` (
+  `id` int(11) NOT NULL,
+  `date_arr` date NOT NULL,
+  `date_sort` date NOT NULL,
+  `commentaire` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `lit_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 --
 -- Déchargement des données de la table `sejour`
 --
 
 INSERT INTO `sejour` (`id`, `date_arr`, `date_sort`, `commentaire`, `patient_id`, `lit_id`) VALUES
-(1, '2022-11-22', '2022-11-25', 'Test1', 1, 10),
+(1, '2022-11-22', '2022-11-25', 'Zozopopo', 1, 10),
 (2, '2022-11-23', '2022-11-24', 'Test2', 2, 12),
 (3, '2022-11-22', '2022-11-27', 'Test3', 3, 18),
 (4, '2022-11-22', '2022-11-30', 'Test4', 4, 4);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `service`
+--
+
+CREATE TABLE `service` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `service`
+--
+
+INSERT INTO `service` (`id`, `libelle`) VALUES
+(1, 'Cardiologie'),
+(2, 'Radiologie'),
+(3, 'Chirurgie'),
+(4, 'Chirurgie esthétique'),
+(5, 'Oncologie'),
+(6, 'Soins intensifs'),
+(7, 'Urologie'),
+(8, 'Anesthésiologie');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:json)',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `personnel_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`id`, `email`, `roles`, `password`, `personnel_id`) VALUES
-(1, 'olivia.la28@gmail.com', '[]', '$2y$13$TUCmVaU6XD5qsFrYPeqt6uGZSzILTU2aAlj6eyZYPSB.QqsHLDitq', NULL);
+(1, 'olivia.la28@gmail.com', '[\"ROLE_ADMIN\"]', '$2y$13$TUCmVaU6XD5qsFrYPeqt6uGZSzILTU2aAlj6eyZYPSB.QqsHLDitq', NULL),
+(2, 'gaetan.oudshoorn@gmail.com', '[\"ROLE_ADMIN\",\"ROLE_INFIRMIER\"]', '$2y$13$5d7mDjNs337.goyWAx3bXeReMVWpi1Zm6naHU.Lpcw7vU8E9uc9gK', NULL);
 
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `chambre`
+--
+ALTER TABLE `chambre`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_C509E4FFED5CA9E6` (`service_id`);
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `lit`
+--
+ALTER TABLE `lit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_5DDB8E9D9B177F54` (`chambre_id`);
+
+--
+-- Index pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Index pour la table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `personnel`
+--
+ALTER TABLE `personnel`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_A6BCF3DEA832C1C9` (`email_id`);
+
+--
+-- Index pour la table `sejour`
+--
+ALTER TABLE `sejour`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_96F520286B899279` (`patient_id`),
+  ADD KEY `IDX_96F52028278B5057` (`lit_id`);
+
+--
+-- Index pour la table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`),
+  ADD UNIQUE KEY `UNIQ_8D93D6491C109075` (`personnel_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `chambre`
+--
+ALTER TABLE `chambre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT pour la table `lit`
+--
+ALTER TABLE `lit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `personnel`
+--
+ALTER TABLE `personnel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `sejour`
+--
+ALTER TABLE `sejour`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `service`
+--
+ALTER TABLE `service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `chambre`
+--
+ALTER TABLE `chambre`
+  ADD CONSTRAINT `FK_C509E4FFED5CA9E6` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`);
+
+--
+-- Contraintes pour la table `lit`
+--
+ALTER TABLE `lit`
+  ADD CONSTRAINT `FK_5DDB8E9D9B177F54` FOREIGN KEY (`chambre_id`) REFERENCES `chambre` (`id`);
+
+--
+-- Contraintes pour la table `personnel`
+--
+ALTER TABLE `personnel`
+  ADD CONSTRAINT `FK_A6BCF3DEA832C1C9` FOREIGN KEY (`email_id`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `sejour`
+--
+ALTER TABLE `sejour`
+  ADD CONSTRAINT `FK_96F52028278B5057` FOREIGN KEY (`lit_id`) REFERENCES `lit` (`id`),
+  ADD CONSTRAINT `FK_96F520286B899279` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`);
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `FK_8D93D6491C109075` FOREIGN KEY (`personnel_id`) REFERENCES `personnel` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
